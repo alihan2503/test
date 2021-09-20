e27cc95df25fed7aafbacc306ae56bfe9c3f9d70
alihan2503 <alihanmaksytaliev@gmail.com>
2021-09-20 19:38:28 +0600

    test

---
+++ python.py
@@ -0,0 +1,16 @@
+class Car:
+    def __init__(self, make, model, year, odometer, fuel):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer = 0
+        self.fuel = 70
+
+
+        
+    
+
+
+    
+
+    
